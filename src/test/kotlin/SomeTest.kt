cc977e865a9a8bd5ab86f7b14d49ca7be6f1cbab
Ferdinand Ade <ferdinand.ade@otto.de>
2022-09-15 18:42:58 +0200

    finished kata

--- src/test/kotlin/SomeTest.kt
+++ src/test/kotlin/SomeTest.kt
@@ -1,17 +1,27 @@
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import kotlin.test.assertEquals
 
 class SomeTest {
 
     @Test
-    fun `test`() {
-        // given
-
-        // when
-
-        // then
-        fail("nope!")
+    fun `fizzBuzz`() {
+        assertEquals("1", fizzBuzz(1))
+        assertEquals("2", fizzBuzz(2))
+        assertEquals("Fizz", fizzBuzz(3))
+        assertEquals("Fizz", fizzBuzz(6))
+        assertEquals("Buzz", fizzBuzz(5))
+        assertEquals("Buzz", fizzBuzz(10))
+        assertEquals("FizzBuzz", fizzBuzz(15))
     }
+
+    private fun fizzBuzz(number: Int): String {
+        if (number.dividableBy(3) && number.dividableBy(5)) return "FizzBuzz"
+        if (number.dividableBy(3)) return "Fizz"
+        if (number.dividableBy(5)) return "Buzz"
+        return number.toString()
+    }
+
+    private fun Int.dividableBy(divider: Int) = this % divider == 0
 }
 
 
