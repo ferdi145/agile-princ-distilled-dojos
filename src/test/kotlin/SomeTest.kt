3d3c37541b16b72e0306b93af9b5458c2fbe4525
Ferdinand Ade <ferdinand.ade@otto.de>
2022-09-15 18:16:45 +0200

    initial commit

---
+++ src/test/kotlin/SomeTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+class SomeTest {
+
+    @Test
+    fun `test`() {
+        // given
+
+        // when
+
+        // then
+        fail("nope!")
+    }
+}
+
+
