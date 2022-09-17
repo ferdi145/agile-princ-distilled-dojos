0eb37ac6d681f695228cbfd5cea96c41a847b2e7
Ferdinand Ade <ferdinand.ade@otto.de>
2022-09-17 14:29:54 +0200

    finished kata

--- src/test/kotlin/SomeTest.kt
+++ src/test/kotlin/SomeTest.kt
@@ -1,17 +1,54 @@
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class SomeTest {
 
     @Test
-    fun `test`() {
-        // given
-
-        // when
-
-        // then
-        fail("nope!")
+    fun `calculate minimum value`() {
+        assertEquals(1, Calculator(listOf(2, 1, 3, 4)).minimumValue)
     }
+
+    @Test
+    fun `calculate minimum value negative value`() {
+        assertEquals(-3, Calculator(listOf(2, 1, -3, 4)).minimumValue)
+    }
+
+    @Test
+    fun `calculate maximum value positive numbers`() {
+        assertEquals(4, Calculator(listOf(2, 1, 4, 3)).maximumValue)
+    }
+
+    @Test
+    fun `calculate maximum value negative numbers`() {
+        assertEquals(3, Calculator(listOf(2, 1, -4, 3)).maximumValue)
+    }
+
+    @Test
+    fun `calculate input sequence length`() {
+        assertEquals(4, Calculator(listOf(2, 1, -4, 3)).sequenceLength)
+    }
+
+    @Test
+    fun `calculate average value`() {
+        assertEquals(0.5, Calculator(listOf(2, 1, -4, 3)).averageValue)
+    }
+
+    @Test
+    fun `CalculatorInput can't be empty list`() {
+        assertThrows<IllegalArgumentException> { Calculator(listOf()) }
+    }
+}
+
+data class Calculator(val values: List<Int>) {
+    init {
+        require(values.isNotEmpty()) { "needs input" }
+    }
+
+    val sequenceLength get() = values.size
+    val minimumValue get() = values.minOrNull()!!
+    val maximumValue get() = values.maxOrNull()!!
+    val averageValue get() = values.average()
 }
 
 
